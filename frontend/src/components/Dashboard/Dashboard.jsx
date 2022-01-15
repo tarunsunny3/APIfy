5c982c021856b7a1d48231e1aae9ef2853b01b73
Tarun Chinthakindi <tarunsunny3@gmail.com>
2022-01-15 20:06:10 +0000

    added basic dashboard

---
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./DashboardStyles.scss";
+const Dashboard = () => {
+  return <div className="container">Welcome to APIfy DASHBOARD, hi, hey</div>;
+};
+
+export default Dashboard;
