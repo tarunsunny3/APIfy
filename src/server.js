b0e8cdf7fbf1ae33c8d714d2fe09f70b747ce157
tarunsunny3 <tarunsunny3@gmail.com>
2022-02-20 23:33:32 +0530

    directories refactored

--- src/server.js
+++ src/server.js
@@ -41,12 +41,12 @@ db.once('open', () => {
 });
 //Serve  static files
 if(process.env.NODE_ENV == 'production'){
-	app.use(express.static(path.resolve('../frontend', 'build')));
+	app.use(express.static(path.resolve(__dirname,  'frontend', 'build')));
 	app.get('/', (req, res)=>{
-		res.sendFile(path.resolve('../frontend', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname ,'frontend', 'build', 'index.html'));
 	});
 	app.get('/*', (req, res)=>{
-		res.sendFile(path.resolve('../frontend', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname ,'/frontend', 'build', 'index.html'));
 	});
 };
 app.get('/', (req, res) => {
