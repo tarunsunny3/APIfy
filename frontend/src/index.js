bb328a236d833b62bc1d0821909365114b95f947
tarunsunny3 <tarunsunny3@gmail.com>
2022-02-21 00:17:57 +0530

    changed index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import axios from "axios";
 import reportWebVitals from "./reportWebVitals";
 
-axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
+// axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
 axios.defaults.withCredentials = true;
 
 ReactDOM.render(
