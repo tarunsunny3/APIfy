d1b7aa888709a629e38cddfb4b493f5cca5cde8a
tarunsunny3 <tarunsunny3@gmail.com>
2022-02-27 01:19:02 +0530

    added backend url in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import axios from "axios";
 import reportWebVitals from "./reportWebVitals";
 
 // axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
-// axios.defaults.baseURL = "https://apify-mern.herokuapp.com";
+axios.defaults.baseURL = "https://apify-mern.herokuapp.com";
 axios.defaults.withCredentials = true;
 
 ReactDOM.render(
